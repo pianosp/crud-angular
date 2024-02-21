e6e8b66f82961b9cb1732e432cb3c4cedc049ca1
supakritpetpon17 <supakritpetpon17@gmail.com>
2024-02-22 02:22:03 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentComponent } from './Pages/department/department/department.component';
+import { UserComponent } from './Pages/User/user/user.component';
+import { DepartmentCreateComponent } from './Pages/department-create/department-create.component';
+import { DepartmentEditComponent } from './Pages/department-edit/department-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: DepartmentComponent, title: 'Department'},
+  {path:'users', component: UserComponent, title: 'User'},
+  {path:'department/create', component: DepartmentCreateComponent, title: 'Create Department'},
+  {path:'department/edit/:id', component: DepartmentEditComponent, title: 'Edit Department'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './Pages/department/department/department.component';
+import { UserComponent } from './Pages/User/user/user.component';
+import { NavbarComponent } from './Pages/Partials/navbar/navbar.component';
+import { DepartmentCreateComponent } from './Pages/department-create/department-create.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { DepartmentEditComponent } from './Pages/department-edit/department-edit.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    UserComponent,
+    NavbarComponent,
+    DepartmentCreateComponent,
+    DepartmentEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
